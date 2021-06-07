eec0a6ac749e24e52b0edc853543fc3f853a497f
Kazuaki Tanaka <kazuaki@mse.kyutech.ac.jp>
2021-06-08 08:19:06 +0900

    サンプルアプリを動かせるようにした

--- README.md
+++ README.md
@@ -1,6 +1,24 @@
-# senstick_check
+# SenStick サンプルアプリケーション
+
+## senstick_app_sample
 
 SenStick の動作を確認するための最も簡単なブラウザアプリ。SenStickアプリを開発する際の参考にもなる。
+
+[senstick_app_sampleを実行する](https://senstick-app.herokuapp.com/)
+
+
+## senstick_app_sensor
+
+SenStick からセンサデータを取得するサンプルアプリケーション。
+
+## senstick_app_mrubyc
+
+SenStick でmruby/cのプログラムを実行するためのアプリケーション。
+
+## senstick_app_view
+
+SenStick の応用アプリケーション例。センサデータをグラフで描画する。
+
 
 
 
--- senstick_app_sample.js
+++ senstick_app_sample.js
@@ -27,6 +27,7 @@ ble.onScan = (deviceName) => {
 
 
 // [scan & connect] ボタン
+// 接続がされると、onConnectGATT が呼ばれる
 const scan_onclick = () => {
     ble.scan('mrubyc').then( () => {
         return ble.connectGATT('mrubyc')
@@ -36,12 +37,60 @@ const scan_onclick = () => {
     })
 }
 
+// GATT接続した
+ble.onConnectGATT = (deviceName) => {
+    document.getElementById('device_status').innerHTML = 'Connected'}
+
 
 // [disconnect] ボタン
+// 切断されると、onDisconnect が呼ばれる
 const disconnect_onclick = () => {
     ble.reset()
 }
 
+
+//　切断処理
+ble.onDisconnect = (deviceName) => {
+    document.getElementById('device_status').innerHTML = 'Disconnected'
+}
+
+
+// [Get Version] ボタンが押されたときの処理
+// mrubycサービスのコマンド02を使う
+const version_onclick = () => {
+    cmd = [0x02]
+    // コマンドを使って値を取り出す
+    ble.write("mrubyc", cmd).then( () => {
+        return ble.read("mrubyc")
+    }).then( (data) => {
+	// コマンドを送信後のデータ取得
+        var ary = new Uint8Array(data.buffer)
+        var i = 0
+        var str = ""
+        while( i<ary.length && ary[i] != 0 ){
+            str += String.fromCharCode(ary[i])
+            i++
+        }
+	// バージョン情報をブラウザに出力する
+        document.getElementById('version_text').innerHTML = str
+    })
+}
+
+
+
+// [LED Off] ボタン
+// mrubycサービスのコマンド00を使う
+const led_off_onclick = () => {
+    cmd = [0x00]
+    ble.write("mrubyc", cmd)
+}
+
+// [LED On] ボタン
+// mrubycサービスのコマンド01を使う
+const led_on_onclick = () => {
+    cmd = [0x01]
+    ble.write("mrubyc", cmd)
+}
 
 
 
